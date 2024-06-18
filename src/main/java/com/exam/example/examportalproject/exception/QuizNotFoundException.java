e9d25edd56466d8d03948574b3bbcea5c4764113
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-06-18 08:36:02 +0530

    resolve issue related delete  quiz

---
+++ src/main/java/com/exam/example/examportalproject/exception/QuizNotFoundException.java
@@ -0,0 +1,10 @@
+/*
+* Author Name: 
+* Date: 6/18/2024
+* Created With: IntelliJ IDEA Community Edition
+*/
+
+
+
+package com.exam.example.examportalproject.exception;public class QuizNotFoundException {
+}
